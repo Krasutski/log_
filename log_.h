f0e1075319058fb868156343c9d29ff05e920d74
DenisKrasutsi <krasutski.denis@gmail.com>
2019-05-04 13:48:04 +0300

    Enable format cheking by GCC compiler

--- log_.h
+++ log_.h
@@ -35,12 +35,12 @@ extern "C" {
 /* ===== TYPEDEFS =========================================================== */
 
 typedef enum log_level_e {
-    LOG_LEVEL_OFF = 0x00,
-    LOG_LEVEL_DEBUG = 0x01,
-    LOG_LEVEL_INFO = 0x02,
+    LOG_LEVEL_OFF =     0x00,
+    LOG_LEVEL_DEBUG =   0x01,
+    LOG_LEVEL_INFO =    0x02,
     LOG_LEVEL_WARNING = 0x04,
-    LOG_LEVEL_ERROR = 0x08,
-    LOG_LEVEL_ALL = 0xFF,
+    LOG_LEVEL_ERROR =   0x08,
+    LOG_LEVEL_ALL =     0xFF,
 } log_level_t;
 
 typedef enum {
@@ -51,37 +51,37 @@ typedef enum {
 /* ===== LOG MACROS ========================================================= */
 
 #if LOG_ENABLED == 1U
-#  define LOG(...)                    log_it( __VA_ARGS__)
-#  define LOG_ARRAY(...)              log_array( __VA_ARGS__)
+#  define LOG(...)                      log_it( __VA_ARGS__)
+#  define LOG_ARRAY(...)                log_array( __VA_ARGS__)
 #else /* LOG_ENABLED == 1 */
 #  define LOG(...)
 #  define LOG_ARRAY(...)
 #endif /* LOG_ENABLED == 1 */
 
 #if LOG_ENABLED_COLOR == 1
-#  define LOG_COLOR_RED              "91"
-#  define LOG_COLOR_GREEN            "92"
-#  define LOG_COLOR_YELLOW           "93"
-#  define LOG_COLOR_BLUE             "94"
-#  define LOG_COLOR_PURPLE           "95"
-#  define LOG_COLOR_WHITE            "97"
-#  define LOG_COLOR(COLOR)           "\033[0;" COLOR "m"
-#  define LOG_BOLD(COLOR)            "\033[1;" COLOR "m"
-#  define LOG_RESET_COLOR()           "\033[0m"
+#  define LOG_COLOR_RED                 "91"
+#  define LOG_COLOR_GREEN               "92"
+#  define LOG_COLOR_YELLOW              "93"
+#  define LOG_COLOR_BLUE                "94"
+#  define LOG_COLOR_PURPLE              "95"
+#  define LOG_COLOR_WHITE               "97"
+#  define LOG_COLOR(COLOR)              "\033[0;" COLOR "m"
+#  define LOG_BOLD(COLOR)               "\033[1;" COLOR "m"
+#  define LOG_RESET_COLOR()             "\033[0m"
 #else
 #  define LOG_COLOR(COLOR)
 #  define LOG_BOLD(COLOR)
 #endif /* LOG_ENABLED_COLOR == 1 */
 
-#define LOG_DEBUG(...)                  LOG(LOG_LEVEL_DEBUG, LOG_COLOR(LOG_COLOR_WHITE)  __VA_ARGS__)
+#define LOG_DEBUG(...)                  LOG(LOG_LEVEL_DEBUG, LOG_COLOR(LOG_COLOR_WHITE) __VA_ARGS__)
 #define LOG_INFO(...)                   LOG(LOG_LEVEL_INFO, LOG_COLOR(LOG_COLOR_GREEN) __VA_ARGS__)
 #define LOG_WARNING(...)                LOG(LOG_LEVEL_WARNING, LOG_COLOR(LOG_COLOR_YELLOW) __VA_ARGS__)
-#define LOG_ERROR(...)                  LOG(LOG_LEVEL_ERROR, LOG_COLOR(LOG_COLOR_RED)  __VA_ARGS__)
+#define LOG_ERROR(...)                  LOG(LOG_LEVEL_ERROR, LOG_COLOR(LOG_COLOR_RED) __VA_ARGS__)
 
 #define LOG_DEBUG_ARRAY(...)            LOG_ARRAY(LOG_LEVEL_DEBUG, LOG_COLOR(LOG_COLOR_WHITE) __VA_ARGS__)
 #define LOG_DEBUG_ARRAY_RED(...)        LOG_ARRAY(LOG_LEVEL_DEBUG, LOG_COLOR(LOG_COLOR_RED) __VA_ARGS__)
 #define LOG_DEBUG_ARRAY_GREEN(...)      LOG_ARRAY(LOG_LEVEL_DEBUG, LOG_COLOR(LOG_COLOR_GREEN) __VA_ARGS__)
-#define LOG_DEBUG_ARRAY_BLUE(...)       LOG_ARRAY(LOG_LEVEL_DEBUG, LOG_COLOR(LOG_COLOR_BLUE) __VA_ARGS
+#define LOG_DEBUG_ARRAY_BLUE(...)       LOG_ARRAY(LOG_LEVEL_DEBUG, LOG_COLOR(LOG_COLOR_BLUE) __VA_ARGS__)
 
 
 #if LOG_ENABLED == 1U
@@ -99,7 +99,12 @@ typedef struct {
 log_result_t log_init(const log_level_t level, log_io_t const *io);
 
 /* Do not use it in your code, better use defines like LOG_INFO or LOG_DEBUG_ARRAY */
-void log_it(const log_level_t level, const char* format, ...);
+void log_it(const log_level_t level, const char* format, ...)
+#if defined (__GNUC__)
+/* Enable format cheking by compiler */
+__attribute__((format(printf, 1, 2)))
+#endif
+;
 void log_array(const log_level_t level, char *message, const uint8_t* array, size_t size);
 #endif // LOG_ENABLED==1U
 
